93dcc01dc361e6a8e9c50f08681597ff4db66705
anweshcr7 <anweshcr7@gmail.com>
2019-11-17 21:51:06 +0100

    upkeep commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import HighchartsReact from 'highcharts-react-official'
 import axios from 'axios'
 import ButtonAppBar from './AppBar'
 import FloatingActionButtons from './FloatingActionButtons'
-import { AppBar } from '@material-ui/core';
+import { Switch } from '@material-ui/core';
+//import btn from './Button'
 
 class App extends Component {
   constructor(props) {
@@ -15,6 +16,8 @@ class App extends Component {
       realData: [],
       realData2: [],
       predictionData:[],
+      predictionChartData: [],
+      switch: true,
       day: 1,
       // To avoid unnecessary update keep all options in the state.
       chartOptions: {
@@ -47,8 +50,14 @@ class App extends Component {
           }
         },
         series: [
-          { name: "Analysis", data: [] }
-        ]
+          { name: "Analysis", data: [], color: "#03a9f4" }
+        ],
+        plotOptions: {
+          areaspline: {
+              fillOpacity: 0.5
+          }
+      }
+
       },
       chartOptions2: {
         chart: {
@@ -83,14 +92,10 @@ class App extends Component {
           { name: "Without Awareness" ,data: [], color: "#aeb7ba" }
         ],
         plotOptions: {
-          series: {
-            point: {
-              events: {
-                mouseOver: this.setHoverData.bind(this)
-              }
-            }
+          areaspline: {
+              fillOpacity: 0
           }
-        }
+      }
       },
       chartOptionsP: {
         chart: {
@@ -134,6 +139,7 @@ class App extends Component {
       },
       hoverData: null
     };
+    
   }
 
   componentDidMount() {
@@ -183,7 +189,7 @@ class App extends Component {
       },
       chartOptionsP: {
         series: [
-          { name: 'Predicted', data: this.state.predictionData },
+          { name: 'Predicted', data: this.state.predictionChartData },
           { name: 'General', data: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1] }
         ]
       }
@@ -192,33 +198,33 @@ class App extends Component {
 
   recordGraph = () => {
     var day = this.state.day;
-    this.getdata(1, '1')
+    this.getdata(1, '20')
   }
   getdataPrediction = (i) =>{
     if(i === 26)
       return 
     else {
-      //axios.get(`http://192.168.43.2:8080/api/getRealData?time=${i}&day=${day}`)
-      axios.get(`https://reqres.in/api/users/2`)
-      .then(res => {
-        var data = this.state.predictionData;
+      
+      //axios.get(`https://reqres.in/api/users/2`)
+        var data = this.state.predictionData.slice(0,i)
         // for(let a = 1; a <= i; a++)
-        data.push(Math.random())
+       // data = data.slice(0,i)
         console.log(data)
-        this.setState({predictionData: data}, this.updateSeries())
+        this.setState({predictionChartData: data}, this.updateSeries())
         //console.log(i)
         i++
-        setTimeout(function(){this.getdataPrediction(i)}.bind(this), 2000)
-      })
+        setTimeout(function(){this.getdataPrediction(i)}.bind(this), 1000)
     }
   }
 
-  playPrediction = () => {
-    var day = this.state.day;
-    this.getdataPrediction(1)
+  showPrediction = () => {
+      axios.get(`http://192.168.43.2:8080/api/getPredictedData`).then(data => {
+      var extrapol_data = data.data.prediction
+      extrapol_data.push(0.6666666666666666, 0.3333333333333333, 0, 0.3333333333333333, 0, 0);
+      console.log(extrapol_data);
+      this.setState({predictionData: extrapol_data}, this.getdataPrediction(1))
+    })
   }
-
-
 
   render() {
     const { chartOptions, hoverData } = this.state;
@@ -227,28 +233,33 @@ class App extends Component {
       <div>
         <ButtonAppBar/>
         <br/>
+        <Switch checked={this.state.switch} onClick={()=>this.setState({switch: !this.state.switch})} value="Switch" />
+        <label>Switch Charts</label>
         <div>
-          {/* <div>
+          {/* <div>btn</div> */}
+          { this.state.switch && <div>
             <br/>
           <HighchartsReact
                 highcharts={Highcharts}
                 options={this.state.chartOptionsP}
               />
-            <FloatingActionButtons type = "play" btnClick={this.playPrediction.bind(this)}/>
-          </div> */}
-          <div>
-              <HighchartsReact
-                highcharts={Highcharts}
-                options={chartOptions}
-              />
-          </div>
-          <FloatingActionButtons type = "record" btnClick={this.recordGraph.bind(this)}/>
-          <div style={{marginTop: "5px"}}>
-              <HighchartsReact
-                highcharts={Highcharts}
-                options={this.state.chartOptions2}
-              />
-          </div>
+            <FloatingActionButtons type = "play" btnClick={this.showPrediction.bind(this)}/>
+          </div>}
+          { this.state.switch == false &&<div>
+            <div>
+                <HighchartsReact
+                  highcharts={Highcharts}
+                  options={chartOptions}
+                />
+            </div>
+            <FloatingActionButtons type = "record" btnClick={this.recordGraph.bind(this)}/>
+            <div style={{marginTop: "5px"}}>
+                <HighchartsReact
+                  highcharts={Highcharts}
+                  options={this.state.chartOptions2}
+                />
+            </div>
+          </div>}
         </div>
       </div>
     )
